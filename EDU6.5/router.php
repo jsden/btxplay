26fb35eface65e7dcbfa0f5e5408b6b8999fe259
Dmitry Shmanatov <dmitry.shmanatov@gmail.com>
2020-07-27 09:13:20 +0400

    Added ajax router

---
+++ EDU6.5/router.php
@@ -0,0 +1,27 @@
+<?php
+$_SERVER['DOCUMENT_ROOT'] = __DIR__;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/local/vendor/autoload.php';
+
+// подключение пролога
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+
+\Bitrix\Main\Loader::includeModule('crm');
+
+switch ($_POST['mode'])
+{
+    case 'update_deal_comment':
+        // Открыть репу
+        $dealRepo = new \Itech\Bitrix\Repo\CRM\DealRepo();
+
+        // Найти сделку
+        if ($deal = $dealRepo->getById($_POST['id'])) {
+            // Записать коммент
+            $deal->UF_CRM_1594815549263 = $_POST['comment'];
+
+            // Сохранить изменения
+            $dealRepo->update($deal);
+        }
+
+        break;
+}
