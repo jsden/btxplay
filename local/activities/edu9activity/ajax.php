a1cf7d57f8ddf626229149f6a9b3b19dd42c84fc
Dmitry Shmanatov <dmitry.shmanatov@gmail.com>
2020-08-12 13:14:15 +0400

    Removed obsolete ajax.php

--- local/activities/edu9activity/ajax.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_SERVER['DOCUMENT_ROOT'] = realpath(__DIR__ . '/../../../');
-
-require_once ($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
-
-\Bitrix\Main\Loader::includeModule('crm');
-
-echo "<tr><td>HI THERE</td></tr>";
